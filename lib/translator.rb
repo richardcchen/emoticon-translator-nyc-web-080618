c24447b16b2b3b18bd77433c2400557dc0c4e31d
Richard Chen <richard.c.chen@gmail.com>
2018-08-02 00:07:59 +0000

    automatically backed up by learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -12,8 +12,8 @@ emoticons.each do |word, symbols|
     jap = symbols[1]
     eng = symbols[0]
     binding.pry
-    hash["get_meaning"][jap] = word
-    hash["get_emoticon"][eng] = jap
+    hash["get_meaning"] = {jap => word}
+    hash["get_emoticon"] = {jap => eng}
    #binding.pry
 end
 
